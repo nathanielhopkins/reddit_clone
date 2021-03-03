f3f4d2db9b7ac86a36e848089c15750b30cd113a
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-03-03 14:52:45 -0800

    write #new, #create, and #destroy in SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_credentials(
+      username: user[username],
+      password: user[password]
+    )
+
+    if user.nil?
+      flash.now[:errors] = "Username and password are incorrect."
+      render :new
+    else
+      login_user!(user)
+      flash[:notices] = "Successfully logged in as #{user.username}!"
+      redirect_to root_url
+    end
   end
 
   def destroy
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    flash[:notices] = "Successfully logged out!"
+
+    redirect_to new_session_url
   end
 end
