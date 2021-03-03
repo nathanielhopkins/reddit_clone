d7e4002b5481ec5fcd781edbf7da4b5f029a8bcf
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-03-03 14:34:56 -0800

    add :session_token to :users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_194804) do
+ActiveRecord::Schema.define(version: 2021_03_03_223257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_03_03_194804) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "session_token", null: false
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
