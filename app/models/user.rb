ae0cd7de0c92c6617fcd9c89cba3918db4625e60
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-03-03 14:43:20 -0800

    add methods and validations to User to handle session_token and search by credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :password_digest, presence: {message: "Password cannot be blank."}
   validates :password, length: { minimum: 6, allow_nil: true}
+  validates :session_token, presence: true
+
+  after_initialize :ensure_session_token
 
   attr_reader :password
 
@@ -12,5 +15,27 @@ class User < ApplicationRecord
 
   def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+
+    return if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
   end
 end
