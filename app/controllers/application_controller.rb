817b4a53aa77aea3b880b9864efd1162384b03f6
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-03-03 14:51:41 -0800

    add #current_user helper method and methods for logging in user and validating login to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+
+	helper_method :current_user, :logged_in?, :require_login!
+
+	private
+	def login_user!(user)
+		session[:session_token] = user.reset_session_token!
+	end
+
+	def current_user
+		return nil unless session[:session_token]
+		@current_user ||= User.find_by(session_token: session[:session_token])
+	end
+
+	def logged_in?
+		!!current_user
+	end
+
+	def require_login!
+		redirect_to new_session_url if !logged_in?
+	end
 end
