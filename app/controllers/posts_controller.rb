060e34119824044aafe32dbd27407dd74ed39aff
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-03-04 15:42:25 -0800

    write methods for PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,53 @@
 class PostsController < ApplicationController
+  before_action :require_author!, only: [:edit, :update]
+
+  def show
+    @post = Post.find_by(id: params[:id])
+    render :show
+  end
+
+  def new
+    @post = Post.new
+    render :new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id == current_user.id
+    
+    if @post.save
+      flash[:notices] = "#{@post.title} posted successfully."
+      redirect_to post_url(@post)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+
+    if @post.update_attributes(post_params)
+      flash[:notices] = "Post updated successfully."
+      redirect_to post_url(@post)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :edit
+    end
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:title, :url, :content, :sub_id)
+  end
+
+  def require_author!
+    @post = Post.find_by(id: params[:id])
+    redirect_to post_url(@post) unless @post.author_id == current_user.id
+  end
 end
