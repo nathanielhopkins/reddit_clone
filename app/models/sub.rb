7df762757e194a95ccc7ef4453c2c2e36a444bda
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-03-04 12:23:19 -0800

    write validations and belongs_to :mod association for Sub class

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -1,2 +1,12 @@
 class Sub < ApplicationRecord
+  validates :title, :description, :moderator_id, presence: true
+  validates :title, uniqueness: true
+
+  belongs_to(
+    :mod,
+    class_name: "User",
+    foreign_key: :moderator_id,
+    primary_key: :id
+    ) 
+    
 end
