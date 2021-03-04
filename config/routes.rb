0b71af342e757d0d8e4e29877f963ecf0c722e98
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-03-04 15:42:07 -0800

    add resources for posts to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
   resources :subs, except: :destroy
+  resources :posts, except: [:destroy, :index]
 end
