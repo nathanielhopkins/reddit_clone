d6377474635823d8bdcbe8ed20216bfe4ee39d5c
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-03-05 12:51:00 -0800

    write validations and associations for PostSub

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,13 @@
 class Post < ApplicationRecord
   validates :title, :sub_id, :author_id, presence: true
+  validates :subs, presence: true
 
-  belongs_to :sub
   belongs_to(
     :author,
     class_name: "User",
     foreign_key: :author_id,
     primary_key: :id
   )
+  has_many :post_subs
+  has_many :subs, through: :post_subs
 end
