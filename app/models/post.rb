6fbe553374dca7fd6cf31c2b5d6a2b209de8ee44
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-03-05 11:47:21 -0800

    fix typos in Sub and Post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   validates :title, :sub_id, :author_id, presence: true
 
   belongs_to :sub
-  belongs_to (
+  belongs_to(
     :author,
     class_name: "User",
     foreign_key: :author_id,
