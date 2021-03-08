3b4de980cf40aefa61f6a47a33f4c1232d9798af
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-03-08 10:47:48 -0800

    write #new and #create in CommentsController ; ;

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+    render :new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.author_id = current_user.id
+    @comment.post_id = params[:post_id]    
+
+    if @comment.save
+      flash[:notices] = "Comment saved successfully!"
+      redirect_to post_url(@comment.post)
+    else
+      flash.now[:errors] = @comment.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
