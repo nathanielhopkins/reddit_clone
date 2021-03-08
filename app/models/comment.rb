7974c31c964686f182eb45799e415c5069b31728
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-03-08 10:43:01 -0800

    write validations and associations for Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,11 @@
 class Comment < ApplicationRecord
+  validates :content, :author, :post, presence: true
+
+  belongs_to :post
+  belongs_to(
+    :author,
+    class_name: "User",
+    foreign_key: :author_id,
+    primary_key: :id
+  )
 end
