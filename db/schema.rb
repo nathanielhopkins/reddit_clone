f69e45c67aecd8fdbc0416516addddf0eab016e2
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-03-08 11:51:45 -0800

    create and run migration to add :parent_comment_id column to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_183616) do
+ActiveRecord::Schema.define(version: 2021_03_08_194855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_183616) do
     t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "parent_comment_id"
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
